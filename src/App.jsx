7243082bc455545fd2e88eb732f26aac78f31619
Trickster2001 <keyurmurkar@gmail.com>
2024-04-21 12:42:52 +0530

    background color changer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
 
 
 function App() {
 
+  const [color, setColor] = useState("black")
+
+
   return (
     <>
-      <h1 className="text-red-300">hello world</h1>
+    <div className="bg-red-500 w-full h-[100vh] flex justify-center items-end" style={{backgroundColor: color}}>
+      <div className="bg-white h-fit w-fit mb-10 rounded-lg">
+      <button onClick={()=>setColor("red")} className='bg-slate-400 p-2 m-3 rounded-lg' style={{ backgroundColor: 'red' }}>Red</button>
+      <button onClick={()=>setColor("green")} className='bg-slate-400 p-2 m-3 rounded-lg' style={{ backgroundColor: 'green' }}>Green</button>
+      <button onClick={()=>setColor("blue")} className='bg-slate-400 p-2 m-3 rounded-lg' style={{ backgroundColor: 'blue' }}>Blue</button>
+      </div>
+    </div>
     </>
   )
 }
