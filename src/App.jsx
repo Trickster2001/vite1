663bbc33b5290fdd194d3be54552d736dcb1d3e5
Trickster2001 <keyurmurkar@gmail.com>
2024-04-21 13:48:07 +0530

    complex password generator created

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,94 @@
-import { useState } from "react";
-
+import { useCallback, useEffect, useRef, useState } from "react";
 
 function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
 
-  const [color, setColor] = useState("black")
+  // useRef hook
+  const passwordRef = useRef(null);
 
+  const passGen = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*(){}[]~`";
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
+
+  useEffect(()=>{passGen()},[length, numberAllowed, charAllowed, passGen]);
+
+  const copyPass = useCallback(()=>{
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0,3);
+    window.navigator.clipboard.writeText(password);
+  },[password])
 
   return (
     <>
-    <div className="bg-red-500 w-full h-[100vh] flex justify-center items-end" style={{backgroundColor: color}}>
-      <div className="bg-white h-fit w-fit mb-10 rounded-lg">
-      <button onClick={()=>setColor("red")} className='bg-slate-400 p-2 m-3 rounded-lg' style={{ backgroundColor: 'red' }}>Red</button>
-      <button onClick={()=>setColor("green")} className='bg-slate-400 p-2 m-3 rounded-lg' style={{ backgroundColor: 'green' }}>Green</button>
-      <button onClick={()=>setColor("blue")} className='bg-slate-400 p-2 m-3 rounded-lg' style={{ backgroundColor: 'blue' }}>Blue</button>
+      <div className="w-full h-full max-w-md mx-auto shadow-md rounded-lg px-4 py-4 my-8 text-orange-500 bg-gray-800">
+        <h1 className="text-white text-center my-3">Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            placeholder="password"
+            readOnly
+            className="outline-none w-full py-1 px-3"
+            ref={passwordRef}
+          />
+          <button onClick={copyPass} className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+            Copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
+            />
+            <label htmlFor="">Length: {length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="charInput"
+              onChange={() => {
+                setCharAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="charInput">Characters</label>
+          </div>
+        </div>
       </div>
-    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
